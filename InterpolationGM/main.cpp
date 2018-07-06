86ed83ceb7467c4a54954345d1e56f8dfaf3f71b
Даниил Волошин <kuravasic@gmail.com>
2018-07-06 14:13:32 +0300

    Initial Commit

---
+++ InterpolationGM/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  InterpolationGM
+//
+//  Created by Даниил Волошин on 7/6/18.
+//  Copyright © 2018 Даниил Волошин. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
